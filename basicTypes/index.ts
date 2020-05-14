ead88761490fd6bf15a0115a9062090c7eec3f38
Sambo Chea <sombochea100@gmail.com>
2020-05-14 08:07:54 +0700

    add sample logs

---
+++ basicTypes/index.ts
@@ -0,0 +1,3 @@
+export class BasicType {
+    numberValue: number
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Run Basic Types
+import { BasicType } from "./basicTypes"
+
+const l = new BasicType()
+l.numberValue = 10
+
+console.log(l)
